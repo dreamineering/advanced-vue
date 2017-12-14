477e6030e407ebc27fa899fca52a1238cf4eeb47
Zac Jones <zacjones93@gmail.com>
2017-12-14 08:56:30 -0800

    updated for sandbox

--- App.vue
+++ App.vue
@@ -8,12 +8,12 @@
 import Vue from "vue"
 import Component from "vue-class-component"
 
-@Component({})
-export default class App extends Vue {
+export default Component({})(
+  class App extends Vue {
   message = "Hello from class"
 
   onClick() {
     this.message = "Goodbye"
   }
-}
+})
 </script>
