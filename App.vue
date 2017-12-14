196da3cdef6557ae01b75b380677d91ffccc4d0d
Zac Jones <zacjones93@gmail.com>
2017-12-14 09:33:08 -0800

    updated comment

--- App.vue
+++ App.vue
@@ -8,13 +8,19 @@
 import Vue from "vue"
 import Component from "vue-class-component"
 
-// Same as using @Component({})
-export default Component({})(
-  class App extends Vue {
+class App extends Vue {
   message = "Hello from class"
 
   onClick() {
     this.message = "Goodbye"
   }
-})
+}
+
+/*
+  Codesandbox does not support decorators. This is the alternate
+  "non-decorator" version (which kinda stinks 😅)
+*/
+
+export default Component({})(App)
+
 </script>
